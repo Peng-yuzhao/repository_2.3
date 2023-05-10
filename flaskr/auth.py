cd684934ad1ad40b6fc49f30f040ffbc1b6487b8
thiswind <23764+thiswind@users.noreply.github.com>
2023-05-10 04:01:33 +0000

    SAVE

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -59,7 +59,7 @@ def login():
         if error is None:
             session.clear()
             session['user_id'] = user['id']
-            return redirect('/hello')
+            return redirect(url_for('index'))
 
         flash(error)
 
@@ -81,7 +81,7 @@ def load_logged_in_user():
 @bp.route('/logout')
 def logout():
     session.clear()
-    return redirect('/auth/login')
+    return redirect(url_for('index'))
 
 
 def login_required(view):
